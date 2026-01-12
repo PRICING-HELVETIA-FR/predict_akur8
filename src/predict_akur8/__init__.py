4e576591fa42f33ed648b32cdee7bc3d77c2e214
Julien Ostor <julien.ostor@helvetia.fr>
2026-01-12 17:49:13 +0100

    Exposition des classes NumpyLut et dérivées dans le module

--- src/predict_akur8/__init__.py
+++ src/predict_akur8/__init__.py
@@ -2,5 +2,6 @@
 
 from .Akur8Model import Akur8Model
 from .utils import report_unknown_values
+from .numpy_luts import NumpyCatCatLut, NumpyCatNumLut, NumpyNumNumLut, NumpyCatLut, NumpyNumLut, NumpyLut
 
-__all__ = ["Akur8Model", "report_unknown_values"]
+__all__ = ["Akur8Model", "report_unknown_values", "NumpyCatCatLut", "NumpyCatNumLut", "NumpyNumNumLut", "NumpyCatLut", "NumpyNumLut", "NumpyLut"]
